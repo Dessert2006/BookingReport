c7f52309193f8d1446d73534a263894d7cd363cc
Dessert2006 <kirti@dessertmarine.com>
2025-06-08 11:43:58 +0530

    making it mobile firendly

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import MasterData from "./pages/MasterData";
 import AddBooking from "./pages/AddBooking";
 import Entries from "./pages/Entries";
 import MasterDataManager from "./pages/MasterDataManager";
+import CompletedFiles from "./pages/CompletedFiles";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import CompletedFiles from "./pages/CompletedFiles";
 
 function App() {
   return (
     <Router>
       <div className="container-fluid">
+        {/* Navbar */}
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">Dashboard</Link>
-            <div className="collapse navbar-collapse">
+
+            {/* Hamburger for mobile */}
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            {/* Collapsible nav menu */}
+            <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                   <Link className="nav-link" to="/master">Add Master Data</Link>
@@ -37,9 +53,10 @@ function App() {
           </div>
         </nav>
 
+        {/* Main Content Area */}
         <div className="container mt-4">
           <Routes>
-            <Route path="/" element={<h2>Welcome to the Dashboard! Please select an option from menu.</h2>} />
+            <Route path="/" element={<h2>Welcome to the Dashboard! Please select an option from the menu.</h2>} />
             <Route path="/master" element={<MasterData />} />
             <Route path="/booking" element={<AddBooking />} />
             <Route path="/entries" element={<Entries />} />
@@ -48,6 +65,7 @@ function App() {
           </Routes>
         </div>
 
+        {/* Toast Notifications */}
         <ToastContainer position="bottom-right" autoClose={3000} />
       </div>
     </Router>
